aefd2916c9f06c5d5cc1b9eb65626991bb125390
NotMugil <notmugil01@gmail.com>
2024-11-25 19:24:51 +0530

    style: few ui changes

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,19 +1,20 @@
-import { defineEcConfig } from 'astro-expressive-code'
+import { defineEcConfig } from "astro-expressive-code";
 
 export default defineEcConfig({
   // You can set configuration options here
-  themes: ['github-dark'],
+  themes: ["github-dark"],
   styleOverrides: {
-    borderRadius: '0.5rem',
+    codeFontFamily: "GeistMono",
+    borderRadius: "0.5rem",
     frames: {
-        terminalBackground: '#0a0a0a',
-        terminalTitlebarBackground: '#0a0a0a',
-        shadowColor: '#0a0a0a',
-        editorActiveTabBackground: '#0a0a0a',
-        editorTabBarBackground: '#0a0a0a',
-        editorActiveTabIndicatorTopColor: '#8b5cf6',
-        editorBackground: '#0a0a0a',
+      terminalBackground: "#0a0a0a",
+      terminalTitlebarBackground: "#0a0a0a",
+      shadowColor: "#0a0a0a",
+      editorActiveTabBackground: "#0a0a0a",
+      editorTabBarBackground: "#0a0a0a",
+      editorActiveTabIndicatorTopColor: "#8b5cf6",
+      editorBackground: "#0a0a0a",
     },
-    codeBackground: '#0a0a0a',
+    codeBackground: "#0a0a0a",
   },
-})
+});
