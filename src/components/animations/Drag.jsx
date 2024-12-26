c11f7f5642cee7b593050cc30271c36973245549
NotMugil <notmugil01@gmail.com>
2024-12-26 09:35:10 +0530

    feat: add light theme

--- src/components/animations/Drag.jsx
+++ src/components/animations/Drag.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Drag = ({ children }) => {
   return (
     <motion.div
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
       whileDrag={{ scale: 0.9, rotate: 10 }}
       drag
