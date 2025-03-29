0e272a0f726637631719f2150ef524443b33c22e
NotMugil <notmugil01@gmail.com>
2025-03-29 14:18:44 +0530

    save

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,20 +1,19 @@
 import { defineEcConfig } from "astro-expressive-code";
 
 export default defineEcConfig({
-  // You can set configuration options here
-  themes: ["github-dark"],
-  styleOverrides: {
-    codeFontFamily: "GeistMono",
-    borderRadius: "0.5rem",
-    frames: {
-      terminalBackground: "#0a0a0a",
-      terminalTitlebarBackground: "#0a0a0a",
-      shadowColor: "#0a0a0a",
-      editorActiveTabBackground: "#0a0a0a",
-      editorTabBarBackground: "#0a0a0a",
-      editorActiveTabIndicatorTopColor: "#8b5cf6",
-      editorBackground: "#0a0a0a",
-    },
-    codeBackground: "#0a0a0a",
-  },
+  themes: ["rose-pine-dawn", "rose-pine"],
+  // styleOverrides: {
+  //   codeFontFamily: "GeistMono",
+  //   borderRadius: "0.5rem",
+  //   frames: {
+  //     terminalBackground: "#0a0a0a",
+  //     terminalTitlebarBackground: "#0a0a0a",
+  //     shadowColor: "#0a0a0a",
+  //     editorActiveTabBackground: "#0a0a0a",
+  //     editorTabBarBackground: "#0a0a0a",
+  //     editorActiveTabIndicatorTopColor: "#8b5cf6",
+  //     editorBackground: "#0a0a0a",
+  //   },
+  //   codeBackground: "#0a0a0a",
+  // },
 });
